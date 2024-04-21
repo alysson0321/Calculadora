638ea948da5ef5bb59dc0d22e7263bedad20e851
alysson0321 <alyssonfdbr874@gmai.com>
2024-04-21 13:06:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+let a=0
+let b=0
+
+function n1(){
+    if (a==0){
+        a=1;
+    }
+    else if(b==0){
+        b=1
+    }
+}    
+function n2(){
+    if (a==0){
+        a=2;
+    }
+    else if(b==0){
+        b=2
+    }
+}    
+function n3(){
+    if (a==0){
+        a=3;
+    }
+    else if(b==0){
+        b=3
+    }
+}    
+function n4(){
+    if (a==0){
+        a=4;
+    }
+    else if(b==0){
+        b=4
+    }
+}    
+function n5(){
+    if (a==0){
+        a=5;
+    }
+    else if(b==0){
+        b=5
+    }
+}    
+function n6(){
+    if (a==0){
+        a=6;
+    }
+    else if(b==0){
+        b=6
+    }
+}
+function n7(){
+    if (a==0){
+        a=7;
+    }
+    else if(b==0){
+        b=7
+    }
+}    
+function n8(){
+    if (a==0){
+        a=8;
+    }
+    else if(b==0){
+        b=8
+    }
+}        
+function n9(){
+    if (a==0){
+        a=9;
+    }
+    else if(b==0){
+        b=9
+    }
+}    
+
+function soma(){
+    a = Number(a);
+    b = Number(b);
+    let soma = Number(a+b);
+    alert('A soma dos números é: '+soma+'.');
+    a=0
+    b=0
+}
+function subtracao(){
+    a = Number(a);
+    b = Number(b);
+    let sub = Number(a-b);
+    alert('A subtração dos números é: '+sub+'.');
+    a=0
+    b=0
+}
+function divi(){
+    a = Number(a);
+    b = Number(b);
+    let divi = Number(a/b);
+    alert('A divisão dos números é: '+divi+'.');
+    a=0
+    b=0
+}
+function mult(){
+    a = Number(a);
+    b = Number(b);
+    let mult = Number(a*b);
+    alert('A multiplicação dos números é: '+mult+'.');
+    a=0
+    b=0
+}
